e603567229d475932d1c592b2080035f0c484611
helicopterfly <helicopterfly@helipad.fritz.box>
2015-04-27 18:04:47 +0200

    Implemented mouse scrolling and fixed offset drawing.

--- TrafficSim/src/editor/MapEditor.java
+++ TrafficSim/src/editor/MapEditor.java
@@ -16,8 +16,7 @@ import trafficsim.Map;
  *
  * @author schueler
  */
-class MapEditor implements GameState
-{
+class MapEditor implements GameState {
 
     private final int ID;
     private StateBasedGame sbGame;
@@ -27,164 +26,135 @@ class MapEditor implements GameState
     private int zoom = 10;
     private int xoffset = 0;
     private int yoffset = 0;
-    
-    public MapEditor(int MAINMENU)
-    {
+
+    public MapEditor(int MAINMENU) {
         this.ID = MAINMENU;
     }
 
     @Override
-    public int getID()
-    {
+    public int getID() {
         return ID;
     }
 
     @Override
-    public void init(GameContainer container , StateBasedGame game) throws SlickException
-    {
+    public void init(GameContainer container, StateBasedGame game) throws SlickException {
         map = new Map();
     }
 
     @Override
-    public void render(GameContainer container , StateBasedGame game , Graphics g) throws SlickException
-    {
+    public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
         g.setClip(0, 0, 1200, 800);
-        
-        for(int i=0; i<tempWidth; i++)
-        {
-            g.drawLine((i+xoffset)*zoom , yoffset*zoom, (i+xoffset)*zoom, (tempHeight+yoffset)*zoom);
+
+        for (int i = 0; i <= tempWidth; i++) {
+            g.drawLine(i * zoom + xoffset, yoffset, i * zoom + xoffset, tempHeight * zoom + yoffset);
         }
-        for(int i=0; i<tempHeight; i++)
-        {
-            g.drawLine(xoffset*zoom , (i+yoffset)*zoom, (tempWidth+xoffset)*zoom, (i+yoffset)*zoom);
+        for (int i = 0; i <= tempHeight; i++) {
+            g.drawLine(xoffset, i * zoom + yoffset, tempWidth * zoom + xoffset, i * zoom + yoffset);
         }
     }
 
     @Override
-    public void update(GameContainer container , StateBasedGame game , int delta) throws SlickException
-    {
+    public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
     }
 
     @Override
-    public void enter(GameContainer container , StateBasedGame game) throws SlickException
-    {
+    public void enter(GameContainer container, StateBasedGame game) throws SlickException {
         sbGame = game;
     }
 
     @Override
-    public void leave(GameContainer container , StateBasedGame game) throws SlickException
-    {
+    public void leave(GameContainer container, StateBasedGame game) throws SlickException {
     }
 
     @Override
-    public void mouseWheelMoved(int change)
-    {
+    public void mouseWheelMoved(int change) {
     }
 
     @Override
-    public void mouseClicked(int button , int x , int y , int clickCount)
-    {
+    public void mouseClicked(int button, int x, int y, int clickCount) {
     }
 
     @Override
-    public void mousePressed(int button , int x , int y)
-    {
+    public void mousePressed(int button, int x, int y) {
     }
 
     @Override
-    public void mouseReleased(int button , int x , int y)
-    {
+    public void mouseReleased(int button, int x, int y) {
     }
 
     @Override
-    public void mouseMoved(int oldx , int oldy , int newx , int newy)
-    {
+    public void mouseMoved(int oldx, int oldy, int newx, int newy) {
     }
 
     @Override
-    public void mouseDragged(int oldx , int oldy , int newx , int newy)
-    {
+    public void mouseDragged(int oldx, int oldy, int newx, int newy) {
+        xoffset -= (oldx - newx);
+        yoffset -= (oldy - newy);
     }
 
     @Override
-    public void setInput(Input input)
-    {
+    public void setInput(Input input) {
     }
 
     @Override
-    public boolean isAcceptingInput()
-    {
+    public boolean isAcceptingInput() {
         return true;
     }
 
     @Override
-    public void inputEnded()
-    {
+    public void inputEnded() {
     }
 
     @Override
-    public void inputStarted()
-    {
+    public void inputStarted() {
     }
 
     @Override
-    public void keyPressed(int key , char c)
-    {
+    public void keyPressed(int key, char c) {
     }
 
     @Override
-    public void keyReleased(int key , char c)
-    {
+    public void keyReleased(int key, char c) {
     }
 
     @Override
-    public void controllerLeftPressed(int controller)
-    {
+    public void controllerLeftPressed(int controller) {
     }
 
     @Override
-    public void controllerLeftReleased(int controller)
-    {
+    public void controllerLeftReleased(int controller) {
     }
 
     @Override
-    public void controllerRightPressed(int controller)
-    {
+    public void controllerRightPressed(int controller) {
     }
 
     @Override
-    public void controllerRightReleased(int controller)
-    {
+    public void controllerRightReleased(int controller) {
     }
 
     @Override
-    public void controllerUpPressed(int controller)
-    {
+    public void controllerUpPressed(int controller) {
     }
 
     @Override
-    public void controllerUpReleased(int controller)
-    {
+    public void controllerUpReleased(int controller) {
     }
 
     @Override
-    public void controllerDownPressed(int controller)
-    {
+    public void controllerDownPressed(int controller) {
     }
 
     @Override
-    public void controllerDownReleased(int controller)
-    {
+    public void controllerDownReleased(int controller) {
     }
 
     @Override
-    public void controllerButtonPressed(int controller , int button)
-    {
+    public void controllerButtonPressed(int controller, int button) {
     }
 
     @Override
-    public void controllerButtonReleased(int controller , int button)
-    {
+    public void controllerButtonReleased(int controller, int button) {
     }
-    
+
 }
