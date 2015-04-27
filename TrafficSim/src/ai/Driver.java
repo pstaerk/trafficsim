e47d5138d3999e26c9810cc2906c7a5aa7f6a49f
Iconstrife <franz-duck@gmx.de>
2015-04-27 15:02:27 +0200

    Added DriverData.

--- TrafficSim/src/ai/Driver.java
+++ TrafficSim/src/ai/Driver.java
@@ -15,9 +15,29 @@ public class Driver
     
     
     
-  
+    
+
     public class DriverData
     {
+        public int maxAssholeInex;
+        public int minAssholeIndex;
         
+        public float maxReactionTime;
+        public float minReactionTime;
+
+        /**Set parameters for random generation of Drivers.
+         * 
+         * @param maxAssholeInex
+         * @param minAssholeIndex
+         * @param maxReactionTime
+         * @param minReactionTime 
+         */
+        public DriverData(int maxAssholeInex, int minAssholeIndex, float maxReactionTime, float minReactionTime)
+        {
+            this.maxAssholeInex = maxAssholeInex;
+            this.minAssholeIndex = minAssholeIndex;
+            this.maxReactionTime = maxReactionTime;
+            this.minReactionTime = minReactionTime;
+        }
     }
 }
