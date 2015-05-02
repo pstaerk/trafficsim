0879f2c2f6fbba2a134edea238d44a8d059091b6
helicopterfly <helicopterfly@helipad.fritz.box>
2015-05-02 21:38:45 +0200

    Merged Editor with Simulator. Editor and Simulator are now both Slick2D-ready and are one program.

--- TrafficSim/src/editor/MainMenu.java
+++ TrafficSim/src/editor/MainMenu.java
@@ -15,157 +15,128 @@ import org.newdawn.slick.state.StateBasedGame;
  *
  * @author schueler
  */
-class MainMenu implements GameState
-{
+public class MainMenu implements GameState {
+
     private final int ID;
     private StateBasedGame sbGame;
-    
-    public MainMenu(int MAINMENU)
-    {
+
+    public MainMenu(int MAINMENU) {
         this.ID = MAINMENU;
     }
 
     @Override
-    public int getID()
-    {
+    public int getID() {
         return ID;
     }
 
     @Override
-    public void init(GameContainer container , StateBasedGame game) throws SlickException
-    {
+    public void init(GameContainer container, StateBasedGame game) throws SlickException {
     }
 
     @Override
-    public void render(GameContainer container , StateBasedGame game , Graphics g) throws SlickException
-    {
+    public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
     }
 
     @Override
-    public void update(GameContainer container , StateBasedGame game , int delta) throws SlickException
-    {
+    public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
     }
 
     @Override
-    public void enter(GameContainer container , StateBasedGame game) throws SlickException
-    {
+    public void enter(GameContainer container, StateBasedGame game) throws SlickException {
         sbGame = game;
     }
 
     @Override
-    public void leave(GameContainer container , StateBasedGame game) throws SlickException
-    {
+    public void leave(GameContainer container, StateBasedGame game) throws SlickException {
     }
 
     @Override
-    public void mouseWheelMoved(int change)
-    {
+    public void mouseWheelMoved(int change) {
     }
 
     @Override
-    public void mouseClicked(int button , int x , int y , int clickCount)
-    {
+    public void mouseClicked(int button, int x, int y, int clickCount) {
     }
 
     @Override
-    public void mousePressed(int button , int x , int y)
-    {
+    public void mousePressed(int button, int x, int y) {
     }
 
     @Override
-    public void mouseReleased(int button , int x , int y)
-    {
+    public void mouseReleased(int button, int x, int y) {
     }
 
     @Override
-    public void mouseMoved(int oldx , int oldy , int newx , int newy)
-    {
+    public void mouseMoved(int oldx, int oldy, int newx, int newy) {
     }
 
     @Override
-    public void mouseDragged(int oldx , int oldy , int newx , int newy)
-    {
+    public void mouseDragged(int oldx, int oldy, int newx, int newy) {
     }
 
     @Override
-    public void setInput(Input input)
-    {
+    public void setInput(Input input) {
     }
 
     @Override
-    public boolean isAcceptingInput()
-    {
+    public boolean isAcceptingInput() {
         return true;
     }
 
     @Override
-    public void inputEnded()
-    {
+    public void inputEnded() {
     }
 
     @Override
-    public void inputStarted()
-    {
+    public void inputStarted() {
     }
 
     @Override
-    public void keyPressed(int key , char c)
-    {
+    public void keyPressed(int key, char c) {
     }
 
     @Override
-    public void keyReleased(int key , char c)
-    {
+    public void keyReleased(int key, char c) {
     }
 
     @Override
-    public void controllerLeftPressed(int controller)
-    {
+    public void controllerLeftPressed(int controller) {
     }
 
     @Override
-    public void controllerLeftReleased(int controller)
-    {
+    public void controllerLeftReleased(int controller) {
     }
 
     @Override
-    public void controllerRightPressed(int controller)
-    {
+    public void controllerRightPressed(int controller) {
     }
 
     @Override
-    public void controllerRightReleased(int controller)
-    {
+    public void controllerRightReleased(int controller) {
     }
 
     @Override
-    public void controllerUpPressed(int controller)
-    {
+    public void controllerUpPressed(int controller) {
     }
 
     @Override
-    public void controllerUpReleased(int controller)
-    {
+    public void controllerUpReleased(int controller) {
     }
 
     @Override
-    public void controllerDownPressed(int controller)
-    {
+    public void controllerDownPressed(int controller) {
     }
 
     @Override
-    public void controllerDownReleased(int controller)
-    {
+    public void controllerDownReleased(int controller) {
     }
 
     @Override
-    public void controllerButtonPressed(int controller , int button)
-    {
+    public void controllerButtonPressed(int controller, int button) {
     }
 
     @Override
-    public void controllerButtonReleased(int controller , int button)
-    {
+    public void controllerButtonReleased(int controller, int button) {
     }
-    
+
 }
