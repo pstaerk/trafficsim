4d4aa8b449e441cfc04bb1dea614cba4a7c9eb21
schueler <schueler@10.30.122.216>
2015-05-13 11:35:00 +0100

    Initializing...

---
+++ TrafficSim/src/trafficsim/Crossroad.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trafficsim;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author schueler
+ */
+public class Crossroad
+{
+    ArrayList<Line> stops;
+    
+    
+}
---
+++ TrafficSim/src/trafficsim/Line.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trafficsim;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author schueler
+ */
+public class Line
+{
+    private ArrayList<Line> prvLines;
+    private ArrayList<Line> nxtLines;
+    private Line leftLine;
+    private Line rightLine;
+    
+    private ArrayList<Point> points;
+    
+    private int speedLimitCar;
+    private int speedLimitTruck;
+    
+    private ArrayList<Car> cars;
+
+    public Line()
+    {
+        prvLines = new ArrayList();
+        nxtLines = new ArrayList();
+        points = new ArrayList();
+        cars = new ArrayList();
+    }
+    
+}
