e22f90364a6829b8bb2f830d584956d543b77409
schueler <schueler@10.30.122.216>
2015-05-13 12:23:32 +0100

    Modified project structure

--- TrafficSim/src/infrastructure/Line.java
+++ TrafficSim/src/infrastructure/Line.java
@@ -2,10 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package trafficsim;
+package infrastructure;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import trafficsim.Car;
 
 /**
  *
