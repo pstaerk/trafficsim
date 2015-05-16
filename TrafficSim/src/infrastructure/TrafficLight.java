8b659e42a1d69ae1206a0cf484b801418a50b3b0
schueler <schueler@10.30.122.216>
2015-05-16 14:15:41 +0100

    Changed class TrafficLight

--- TrafficSim/src/infrastructure/TrafficLight.java
+++ TrafficSim/src/infrastructure/TrafficLight.java
@@ -4,11 +4,16 @@
  */
 package infrastructure;
 
-/**
+/**This class is the representation of a traffic light. You can turn it on and of.
  *
  * @author schueler
  */
 public class TrafficLight
 {
-    
+    boolean isOn;
+
+    public TrafficLight(boolean isOn)
+    {
+        this.isOn = isOn;
+    }
 }
