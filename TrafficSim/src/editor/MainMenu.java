389cd5e9d9c8e2169e02bc4c7ed7a7730ca8058f
Helicopterfly <schueler@10.30.122.214>
2015-05-20 17:39:00 +0100

    Added Slick2D and Editor. Editor is part of the project and is written for Slick2D

---
+++ TrafficSim/src/editor/MainMenu.java
@@ -0,0 +1,171 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package editor;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.GameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ *
+ * @author schueler
+ */
+class MainMenu implements GameState
+{
+    private final int ID;
+    private StateBasedGame sbGame;
+    
+    public MainMenu(int MAINMENU)
+    {
+        this.ID = MAINMENU;
+    }
+
+    @Override
+    public int getID()
+    {
+        return ID;
+    }
+
+    @Override
+    public void init(GameContainer container , StateBasedGame game) throws SlickException
+    {
+    }
+
+    @Override
+    public void render(GameContainer container , StateBasedGame game , Graphics g) throws SlickException
+    {
+    }
+
+    @Override
+    public void update(GameContainer container , StateBasedGame game , int delta) throws SlickException
+    {
+    }
+
+    @Override
+    public void enter(GameContainer container , StateBasedGame game) throws SlickException
+    {
+        sbGame = game;
+    }
+
+    @Override
+    public void leave(GameContainer container , StateBasedGame game) throws SlickException
+    {
+    }
+
+    @Override
+    public void mouseWheelMoved(int change)
+    {
+    }
+
+    @Override
+    public void mouseClicked(int button , int x , int y , int clickCount)
+    {
+    }
+
+    @Override
+    public void mousePressed(int button , int x , int y)
+    {
+    }
+
+    @Override
+    public void mouseReleased(int button , int x , int y)
+    {
+    }
+
+    @Override
+    public void mouseMoved(int oldx , int oldy , int newx , int newy)
+    {
+    }
+
+    @Override
+    public void mouseDragged(int oldx , int oldy , int newx , int newy)
+    {
+    }
+
+    @Override
+    public void setInput(Input input)
+    {
+    }
+
+    @Override
+    public boolean isAcceptingInput()
+    {
+        return true;
+    }
+
+    @Override
+    public void inputEnded()
+    {
+    }
+
+    @Override
+    public void inputStarted()
+    {
+    }
+
+    @Override
+    public void keyPressed(int key , char c)
+    {
+    }
+
+    @Override
+    public void keyReleased(int key , char c)
+    {
+    }
+
+    @Override
+    public void controllerLeftPressed(int controller)
+    {
+    }
+
+    @Override
+    public void controllerLeftReleased(int controller)
+    {
+    }
+
+    @Override
+    public void controllerRightPressed(int controller)
+    {
+    }
+
+    @Override
+    public void controllerRightReleased(int controller)
+    {
+    }
+
+    @Override
+    public void controllerUpPressed(int controller)
+    {
+    }
+
+    @Override
+    public void controllerUpReleased(int controller)
+    {
+    }
+
+    @Override
+    public void controllerDownPressed(int controller)
+    {
+    }
+
+    @Override
+    public void controllerDownReleased(int controller)
+    {
+    }
+
+    @Override
+    public void controllerButtonPressed(int controller , int button)
+    {
+    }
+
+    @Override
+    public void controllerButtonReleased(int controller , int button)
+    {
+    }
+    
+}
