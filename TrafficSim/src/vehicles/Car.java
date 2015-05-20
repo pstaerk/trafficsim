902a165f456202ba45fe22a4a944852bf53a62cb
schueler <schueler@10.30.122.215>
2015-05-20 17:07:13 +0100

    update Car

--- TrafficSim/src/vehicles/Car.java
+++ TrafficSim/src/vehicles/Car.java
@@ -4,8 +4,8 @@
  */
 package vehicles;
 
-import infrastructure.Line;
 import ai.Driver;
+import infrastructure.Line;
 
 /**
  *
@@ -16,6 +16,7 @@ public class Car
     public final int MAX_SPEED;
     public final int MAX_ACCL;
     public final int MAX_DCCL;
+    public final int CAR_LENGTH;
     
     private Line line;
     private Driver driver;
@@ -23,11 +24,12 @@ public class Car
     private float curVelocity;
     private float curAcceleration;
 
-    public Car(int MAX_SPEED , int MAX_ACCL , int MAX_DCCL , Line line , Driver driver , float x , float curVelocity , float curAcceleration)
+    public Car(int MAX_SPEED , int MAX_ACCL , int MAX_DCCL , int CAR_LENGTH , Line line , Driver driver , float x , float curVelocity , float curAcceleration)
     {
         this.MAX_SPEED = MAX_SPEED;
         this.MAX_ACCL = MAX_ACCL;
         this.MAX_DCCL = MAX_DCCL;
+        this.CAR_LENGTH = CAR_LENGTH;
         this.line = line;
         this.driver = driver;
         this.x = x;
