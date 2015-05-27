9f8baa4d1f4b9ac52ab2a8c19167255a38524419
helicopterfly <schueler@10.30.122.214>
2015-05-27 19:39:50 +0100

    Added EditorMenu.

---
+++ TrafficSim/src/gui/EditorMenu.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import gui.elements.Drawable;
+import org.newdawn.slick.Graphics;
+
+/**
+ *
+ * @author schueler
+ */
+public class EditorMenu implements Drawable
+{
+
+    @Override
+    public void draw(Graphics g)
+    {
+        
+    }
+    
+}
